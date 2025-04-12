9ab041458966b438013a7d841b1653dd7947d07c
neerajjain92 <neerajjain1392@gmail.com>
2025-04-13 00:57:28 +0530

    first commit

---
+++ src/main/java/com/neeraj/App.java
@@ -0,0 +1,13 @@
+package com.neeraj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
